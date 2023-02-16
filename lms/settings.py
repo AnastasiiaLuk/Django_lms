8dfefbbde80a94aa011cefc5b97e92435cdfb421
LuAna <98122945+AnastasiiaLuk@users.noreply.github.com>
2023-02-16 16:55:05 +0000

    Update settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -143,10 +143,3 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 INTERNAL_IPS = [
     "127.0.0.1",
  ]
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
-
-INTERNAL_IPS = [
-    "127.0.0.1",
- ]
